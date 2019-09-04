7607e43e19369935c43a0bfe988352abbb83bb49
mmumshad <mmumshad@gmail.com>
2019-09-05 01:42:45 +0800

    updates

--- my-new-role/molecule-test.sh
+++ my-new-role/molecule-test.sh
@@ -7,7 +7,7 @@ SECONDS=0
 
 # docker exec -e TEST_DIRECTORY=tests/tests-3 molecule-tester molecule test --scenario-name playbook-1 --destroy=never
 
-docker exec -e TEST_DIRECTORY=tests/tests-4 molecule-tester molecule prepare --scenario-name playbook-1
+docker exec -e TEST_DIRECTORY=tests/tests-4 molecule-tester molecule cleanup --scenario-name playbook-1
 docker exec -e TEST_DIRECTORY=tests/tests-4 molecule-tester molecule converge --scenario-name playbook-1
 docker exec -e TEST_DIRECTORY=tests/tests-4 molecule-tester molecule verify --scenario-name playbook-1
 
